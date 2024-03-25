f6821207fb9bc2a88242b21786ec37cd3d1506fc
Paula Biazotto <pds_paula@hotmail.com>
2024-03-25 19:46:53 +0000

    WIP: started ex01

---
+++ CPP00/ex01/Contact.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/25 19:40:56 by paula             #+#    #+#             */
+/*   Updated: 2024/03/25 19:46:19 by paula            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+class Contact
+{
+    public:
+
+    private:
+        std::string m_first_name;
+        std::string m_last_name;
+        std::string m_nickname;
+        std::string m_phone_number;
+        std::string m_darkest_secret;
+};
