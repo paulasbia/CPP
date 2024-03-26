04ef68dd301cb681f51372f6c7a2a936bc843f2e
Paula Biazotto <pds_paula@hotmail.com>
2024-03-26 10:18:05 +0000

    WIP:main.cpp

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,13 +6,40 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 20:20:20 by paula             #+#    #+#             */
-/*   Updated: 2024/03/25 20:23:06 by paula            ###   ########.fr       */
+/*   Updated: 2024/03/26 10:04:53 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Contact.hpp"
+#include "PhoneBook.hpp"
+
+int    is_valid(std::string &choice)
+{
+    if (choice == "ADD" || choice == "SEARCH" || choice == "EXIT")
+        return 1;
+    else
+        return 0;
+}
+
+void    prompt_choice(std::string &choice)
+{
+    std::cout << "Please, select one of this options: ADD, SEARCH or EXIT: " << '\n';
+    while (!is_valid(choice))
+    {
+        std::cout << "> ";
+        std::cin >> choice;
+    }
+}
 
 int main(void)
 {
-    std::cout << "Welcome, chose onde of three commands: ADD, SEARCH or EXIT:" << '\n';
+    PhoneBook phone_book;
+    std::string choice{};
+    
+    std::cout << "Welcome to PhoneBook!!" << '\n';
+    while (1)
+    {
+        choice.clear();
+        prompt_choice(choice);
+        std::cout << choice << '\n';
+    }
 }
