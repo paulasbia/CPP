493f3540cf0f081677de698f1919a78e5fb827b0
Paula Biazotto <pds_paula@hotmail.com>
2024-03-30 13:52:56 +0000

    feat: displayTime

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -6,11 +6,18 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 11:51:31 by paula             #+#    #+#             */
-/*   Updated: 2024/03/30 12:40:53 by paula            ###   ########.fr       */
+/*   Updated: 2024/03/30 13:51:51 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <ctime>
+#include <iostream>
+
+int				Account::_nbAccounts = 0;
+int				Account::_totalAmount = 0;
+int				Account::_totalNbDeposits = 0;
+int				Account::_totalNbWithdrawals = 0;
 
 Account::Account( void )
 {
@@ -18,11 +25,14 @@ Account::Account( void )
 
 Account::Account( int initial_deposit )
 {
+    _displayTimestamp();
+    _totalAmount = initial_deposit;
     
 }
 
 Account::~Account( void )
 {
+    
 }
 
 int	Account::getNbAccounts( void )
@@ -47,22 +57,29 @@ int	Account::getNbWithdrawals( void )
 
 void	Account::displayAccountsInfos( void )
 {
-    
+        _displayTimestamp();
 }
 
 void	Account::makeDeposit( int deposit )
 {
-    
+        _totalNbDeposits += 1;
+        _totalAmount += deposit;
 }
 
 bool	Account::makeWithdrawal( int withdrawal )
 {
-    
+    if (withdrawal > _totalAmount)
+        return false;
+    else
+    {
+        _totalAmount -= withdrawal;
+        return true;
+    }
 }
 
 int		Account::checkAmount( void ) const
 {
-    
+    return 0;
 }
 
 void	Account::displayStatus( void ) const
@@ -70,7 +87,17 @@ void	Account::displayStatus( void ) const
     
 }
 
+// Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
+// for more information about date/time format
 void	Account::_displayTimestamp( void )
-{
-    
+{   
+    time_t now = time(0); // get current date and time  
+
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+    strftime(buf, sizeof(buf), "%Y%m%d_%H%M%S", &tstruct);
+    std::cout << "[";
+    std::cout << buf;
+    std::cout << "]";
 }
