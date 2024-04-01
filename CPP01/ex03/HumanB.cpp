ebf24b4e81006a960e100c1d2f6658b6f15d8720
Paula Biazotto <pds_paula@hotmail.com>
2024-04-01 17:48:35 +0100

    fix: weapon now getting weapon name

--- CPP01/ex03/HumanB.cpp
+++ CPP01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 15:28:00 by paula             #+#    #+#             */
-/*   Updated: 2024/04/01 17:43:11 by paula            ###   ########.fr       */
+/*   Updated: 2024/04/01 17:47:51 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void    HumanB::attack( void )
     if(!m_weapon)
         std::cout << m_name << " is unarmed and defenseless" << std::endl;
     else
-        std::cout << m_name << " attacks with their " << m_weapon << std::endl;
+        std::cout << m_name << " attacks with their " << m_weapon->getType() << std::endl;
 }
 
 void    HumanB::setWeapon(Weapon &weapon)
