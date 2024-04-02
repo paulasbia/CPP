12e550a64f5b90df2dc67a0f15d761581b9d376d
Paula Biazotto <pds_paula@hotmail.com>
2024-04-02 13:35:45 +0100

    chore:add ope error

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 10:55:56 by paula             #+#    #+#             */
-/*   Updated: 2024/04/02 13:24:37 by paula            ###   ########.fr       */
+/*   Updated: 2024/04/02 13:35:11 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,18 @@ void    print_error(std::string msg)
 int main(int ac, char **av)
 {
    // char            c;
-    //std::ifstream   infile;
+    std::ifstream   infile;
     //std::string     str;
 
     if (ac != 4 || !av[1][0] || !av[2][0])
     {
-        print_error("invalid args: use ./sad_is_for_losers <file> <old_word> <new_word>");
+        print_error("Invalid args: use ./sad_is_for_losers <file> <old_word> <new_word>");
 		return (1);
     }
-    
+    infile.open(av[1]);
+    if(infile.fail())
+    {
+        print_error("No such file or directory");
+		return (1);
+    }
 }
