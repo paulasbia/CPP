4a964de34d3ad1f2a0c98c0a1b399885c836dfdc
Paula Biazotto <pds_paula@hotmail.com>
2024-04-02 10:27:00 +0100

    chore: add main test

--- CPP01/ex03/main.cpp
+++ CPP01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 13:12:08 by paula             #+#    #+#             */
-/*   Updated: 2024/04/01 17:42:52 by paula            ###   ########.fr       */
+/*   Updated: 2024/04/02 10:26:41 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,27 @@
 
 int main()
 {
-    HumanB  josh = HumanB("Josh");
-    Weapon  club = Weapon("crude spiked club");
-    josh.attack();
-    josh.setWeapon(club);
-    josh.attack();
+    {
+        HumanB  josh = HumanB("Josh");
+        Weapon  club = Weapon("crude spiked club");
+        josh.attack();
+        josh.setWeapon(club);
+        josh.attack();
+    }
+    // {
+    //     Weapon club = Weapon("crude spiked club");
+    //     HumanA bob("Bob", club);
+    //     bob.attack();
+    //     club.setType("some other type of club");
+    //     bob.attack();
+    // }
+    {
+        Weapon club = Weapon("crude spiked club");
+        HumanB jim("Jim");
+        jim.setWeapon(club);
+        jim.attack();
+        club.setType("some other type of club");
+        jim.attack();
+    }
     return 0;
 }
