246165d376dd39cb9c1c303fd1ce45a10261bd45
Paula Biazotto <pds_paula@hotmail.com>
2024-04-02 14:16:15 +0100

    feat: implementing find and replace av2 to av3

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 10:55:56 by paula             #+#    #+#             */
-/*   Updated: 2024/04/02 13:35:11 by paula            ###   ########.fr       */
+/*   Updated: 2024/04/02 14:14:34 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,37 @@ void    print_error(std::string msg)
     std::cerr << msg << std::endl;
 }
 
+int ft_replace(char **av, std::string str)
+{
+    std::ofstream   outfile;
+    int             position;
+    
+    outfile.open((std::string(av[1]) + ".replace").c_str());
+    if(outfile.fail())
+    {
+        print_error("No such file or directory");
+		return (1);
+    }
+    for (int i = 0; i < (int)str.size(); i++)
+    {
+        position = str.find(av[2], i);
+		if (position != -1 && position == i)
+		{
+			outfile << av[3];
+			i += std::string(av[2]).size() - 1;
+		}
+		else
+			outfile << str[i];
+    }
+    outfile.close();
+	return (0);
+}
+
 int main(int ac, char **av)
 {
-   // char            c;
+    char            c;
     std::ifstream   infile;
-    //std::string     str;
+    std::string     str;
 
     if (ac != 4 || !av[1][0] || !av[2][0])
     {
@@ -37,4 +63,8 @@ int main(int ac, char **av)
         print_error("No such file or directory");
 		return (1);
     }
+    while (!infile.eof() && infile >> std::noskipws >> c)
+        str += c;
+    infile.close();
+    return(ft_replace(av, str));
 }
