9b145deadde6f4802ea83ea2fdee79113c81322b
Paula Biazotto <pds_paula@hotmail.com>
2024-04-02 13:26:55 +0100

    chore: add error if av is wrong

---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/02 10:55:56 by paula             #+#    #+#             */
+/*   Updated: 2024/04/02 13:24:37 by paula            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <string_view>
+
+void    print_error(std::string msg)
+{
+    std::cerr << "\033[0;31m[ERROR]\033[0m";
+    std::cerr << msg << std::endl;
+}
+
+int main(int ac, char **av)
+{
+   // char            c;
+    //std::ifstream   infile;
+    //std::string     str;
+
+    if (ac != 4 || !av[1][0] || !av[2][0])
+    {
+        print_error("invalid args: use ./sad_is_for_losers <file> <old_word> <new_word>");
+		return (1);
+    }
+    
+}
