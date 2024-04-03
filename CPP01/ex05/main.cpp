5f2e0d3ffadd03fb78bb74a0bc0946c4d786b3ad
Paula Biazotto <pds_paula@hotmail.com>
2024-04-03 15:28:48 +0100

    fix: changed main with ARGV

--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -6,22 +6,33 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 16:07:19 by paula             #+#    #+#             */
-/*   Updated: 2024/04/02 16:20:56 by paula            ###   ########.fr       */
+/*   Updated: 2024/04/03 15:27:31 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main( void )
+// int main( int ac, char **av )
+// {
+//     Harl    say;
+    
+//     if(ac == 2)
+//         say.complain(av[1]);
+//     else
+//         std::cout << "Please choise a complain DEGUG, INFO, WARNING or ERROR\n";
+//     return 0;
+// }
+
+int main( int ac, char **av )
 {
     Harl    say;
     
-    say.complain("DEBUG");
-    say.complain("INFO");
-    say.complain("WARNING");
-    say.complain("ERROR");
-    say.complain("NONE");
-
+    if(ac > 1)
+    {
+        for(int i = 1; i <= (ac - 1); i++)
+             say.complain(av[i]);
+    }
+    else
+        std::cout << "Please choise a complain DEBUG, INFO, WARNING or ERROR\n";
     return 0;
-    
 }
