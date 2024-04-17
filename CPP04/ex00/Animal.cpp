a1987b2aa978a903da63491b33b891cbc4b16d3a
Paula Biazotto <pds_paula@hotmail.com>
2024-04-17 10:38:04 +0100

    WIP: colors

--- CPP04/ex00/Animal.cpp
+++ CPP04/ex00/Animal.cpp
@@ -6,7 +6,7 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 14:04:47 by paula             #+#    #+#             */
-/*   Updated: 2024/04/17 10:35:11 by paula            ###   ########.fr       */
+/*   Updated: 2024/04/17 10:37:27 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 Animal::Animal()
 { 
-    std::cout << GREEN << "Animal default constructor was called" << std::endl << END;
+    std::cout << "Animal default constructor was called" << std::endl << END;
 }
 
 Animal::Animal(const std::string type)
 {
     _type = type; 
-    std::cout << GREEN << "Animal default constructor was called" << std::endl << END;
+    std::cout << "Animal default constructor was called" << std::endl << END;
 }
 
 Animal::Animal(const Animal& copy)
 {
-    std::cout << GREEN << "Animal copy constructor called" << std::endl << END;
+    std::cout << "Animal copy constructor called" << std::endl << END;
     *this = copy;
 }
 
 Animal   &Animal::operator=(const Animal& copy)
 {
-    std::cout << GREEN << "Animal copy assignment operator called" << std::endl << END;
+    std::cout << "Animal copy assignment operator called" << std::endl << END;
     if(this == &copy)
         return *this;
     this->_type = copy._type;
@@ -40,7 +40,7 @@ Animal   &Animal::operator=(const Animal& copy)
 
 Animal::~Animal()
 {
-    std::cout << GREEN << "Animal default destructor" << std::endl << END;
+    std::cout << "Animal default destructor" << std::endl << END;
 }
 
 const std::string      Animal::getType() const
