1de687ca6cdee504a36f0c94c3a1252c627893bb
Paula de Souza Biazotto <paulabiazotto@Marvin-the-Paranoid.local>
2024-06-12 10:15:47 +0100

    feat: added increment and decrement test on the main

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: paulabiazotto <paulabiazotto@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 14:19:39 by paulabiazot       #+#    #+#             */
-/*   Updated: 2024/06/12 10:02:18 by paulabiazot      ###   ########.fr       */
+/*   Updated: 2024/06/12 10:14:53 by paulabiazot      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,12 @@ int main()
 	}
 
 	{
-		std::cout << MAGENTA << "######  Starting valid bureaucrat  ######" << std::endl;
+		std::cout << MAGENTA << "\n######  Starting valid bureaucrat  ######" << std::endl;
 		Bureaucrat b1 = Bureaucrat("Bureaucrat1", 1);
 		Bureaucrat b2 = Bureaucrat("Bureaucrat2", 150);
 		std::cout << b1;
 		std::cout << b2;
-        std::cout << MAGENTA << "######  Starting invalid bureaucrat  ######" << std::endl;
+        std::cout << MAGENTA << "\n######  Starting invalid bureaucrat  ######" << std::endl;
 		try
 		{
 			Bureaucrat b3 = Bureaucrat("Bureaucrat3", 151);
@@ -55,8 +55,35 @@ int main()
 		{
             std::cout << e.what();
 		}
-
-
+    }
+    
+    {
+        std::cout << MAGENTA << "\n######  Starting increment test  ######" << std::endl;
+	    Bureaucrat b1("Bureaucrat1", 2);
+	    b1.incrementGrade();
+	    std::cout << b1 << std::endl;
+	    Bureaucrat b2("Bureaucrat2", 1);
+	    try
+	    {
+	    	b1.incrementGrade();
+	    }
+	    catch (const std::exception& e)
+	    {
+	    	std::cout << e.what() << std::endl;
+	    }
+        std::cout << MAGENTA << "\n######  Starting decrement test  ######" << std::endl;
+	    Bureaucrat b3("Bureaucrat1", 149);
+	    b3.decrementGrade();
+	    std::cout << b3 << std::endl;
+	    Bureaucrat b4("Bureaucrat4", 150);
+	    try
+	    {
+	    	b4.decrementGrade();
+	    }
+	    catch (const std::exception& e)
+	    {
+	    	std::cout << e.what() << std::endl;
+	    }
     }
 	return 0;	
 }
