b15da708f59959c66b0847c876d644be3ea8a80b
Paula de Souza Biazotto <paulabiazotto@Marvin-the-Paranoid.local>
2024-06-12 10:22:54 +0100

    chore: just copy ex00 to ex01

---
+++ CPP05/ex01/Bureaucrat.cpp
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paulabiazotto <paulabiazotto@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 14:19:46 by paulabiazot       #+#    #+#             */
+/*   Updated: 2024/06/12 10:02:59 by paulabiazot      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() 
+    : m_name("Default")
+    , m_grade(150)
+{
+	std::cout << "Bureaucrat default constructor was called" << std::endl << END;
+}
+
+Bureaucrat::Bureaucrat(const std::string name, int grade) 
+    : m_name(name)
+    , m_grade(grade)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	else if (grade > 150)
+		throw GradeTooLowException();
+	m_grade = grade;
+	return ;
+}
+
+// Copy constructor
+Bureaucrat::Bureaucrat(Bureaucrat const &other) 
+    : m_name(other.m_name)
+    , m_grade(other.m_grade)
+{
+	std::cout << "Bureaucrat copy constructor was called" << std::endl << END;
+}
+
+Bureaucrat::~Bureaucrat() 
+{
+	std::cout << "Bureaucrat destructor was called" << std::endl << END;
+}
+// Operator overloads
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &copy)
+{
+    std::cout << "Bureaucrat assignation operator called" << std::endl << END;
+	if (this == &copy)
+		return (*this);
+	this->m_grade = copy.m_grade;
+	return (*this);
+}
+
+std::string const &Bureaucrat::getName() const
+{
+	return (m_name);
+}
+
+int Bureaucrat::getGrade() const
+{
+	return (m_grade);
+}
+
+// Member increment
+void Bureaucrat::incrementGrade()
+{
+	if (--m_grade < 1)
+		throw GradeTooHighException();
+}
+
+void Bureaucrat::decrementGrade()
+{
+	if (++m_grade > 150)
+		throw GradeTooLowException();
+}
+
+// Exception classes
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return (RED "Grade too high!\n" END);
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return (RED "Grade too low!\n" END);
+}
+
+std::ostream &operator<<(std::ostream &out, Bureaucrat const &bureaucrat)
+{
+	out << GREEN << "I am " << bureaucrat.getName() << ", my bureaucrat grade is "
+		<< bureaucrat.getGrade() << std::endl << END;
+	return (out);
+}
