3ce4c212dfda9d492915ffc2dae316c88ad5a706
Paula Biazotto <pds_paula@gotmail.com>
2024-10-07 15:55:10 +0100

    chore:ex00 done

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -7,16 +7,16 @@ int main()
 
     ::swap( a, b);
     std::cout << "a =  " << a << ", b = " << b << std::endl;
-    // std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
-    // std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+    std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+    std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
 
     std::string c = "chaine1";
     std::string d = "chaine2";
 
     ::swap(c, d);
     std::cout << "c = " << c << ", d = " << d << std::endl;
-    // std::cout << "min(c,d) = " << ::min(c, d) << std::endl;
-    // std::cout << "max(c, d ) = " << ::max(c, d) << std::endl;
+    std::cout << "min(c,d) = " << ::min(c, d) << std::endl;
+    std::cout << "max(c, d ) = " << ::max(c, d) << std::endl;
 
     return 0;
 }
--- CPP07/ex00/whatever.hpp
+++ CPP07/ex00/whatever.hpp
@@ -1,15 +1,25 @@
-#ifndef HEADER_HPP
-#define HEADER_HPP
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
 
 #include <iostream>
 
 template<typename T> void swap(T &a, T &b)
 {
-    if(&a ==&b)
+    if(&a == &b)
         return;
     T temp = a;
     a = b;
     b = temp;
+}
+
+template<typename T> T min(T &a, T &b)
+{
+    return((a < b) ? a: b);
+}
+
+template<typename T> T max(T &a, T &b)
+{
+    return((a > b) ? a: b);
 }
 
 #endif
